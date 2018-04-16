b3016f028f694d3a4e66e9134df47e28078accb4
lyj505 <liyingjie@goodsogood.com>
2018-04-16 15:52:23 +0800

    提交单选多选的组件

---
+++ app/web/component/radio-multi-select/radio-multi-select.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import {
+	Button,WingBlank,Checkbox
+} from 'antd-mobile';
+
+const CheckboxItem = Checkbox.AgreeItem
+import PropTypes from 'prop-types';
+
+import './index.less';
+
+
+class RadioMultiSelect extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            voteInfo:null
+        }
+    }
+    
+
+ componentWillMount() {
+    console.log('----componentWillMount')
+   
+ }
+
+ componentDidMount(){
+    console.log('----componentDidMount')
+ }
+ componentWillUnmount(){
+    console.log('----componentWillUnmount')
+ }
+render(){
+    console.log('-----render');
+    let  { voteInfo,checkBoxChange } = this.props;
+    return (
+        <div className="main-vote-wrap radio-multi-select-wrapper">
+            {voteInfo && voteInfo.map( (val,index_p) => {
+                        return (
+                            <div className="body-content" key={index_p}>
+                                <div className="select-row-title">投票选项{val.type === 1 ? '（单选）' : '（多选）'}</div>
+                                {val.list.map( (val_1,index_c) => {
+                                    let classN = 'option' + (index_c===val.list.length-1 ? ' last':'');
+    
+                                    if(val_1.isActive){
+                                        classN += ' active';
+                                    }
+    
+                                    return (
+                                        <div className={classN} key={index_c}>
+                                            <div className="left">
+                                                {val_1.img && <img className="img" src={val_1.img} alt=""/>}
+                                                <p className="title">{val_1.title}</p>
+                                                {val_1.img && (
+                                                    <div className="note">
+                                                        <span>摘要</span>
+                                                        <span>介绍</span>
+                                                        <span>更多</span>
+                                                    </div>
+                                                )}
+                                            </div>
+                                            <div className="right">
+                                                <CheckboxItem onChange={()=>{checkBoxChange(index_p,index_c)}}
+                                                checked={val_1.isActive} style={{ marginTop: -20 }}/>
+                                            </div>
+                                        </div>
+                                    )
+                                } )}
+                            </div>
+                        )
+                    } )}
+        </div>
+      );
+}
+  
+};
+
+// RadioMultiSelect.propTypes = {
+//   show: PropTypes.bool.isRequired,
+//   message: PropTypes.string.isRequired,
+//   confirmHandle:PropTypes.func.isRequired
+// };
+
+RadioMultiSelect.defaultProps = {
+  show: false,
+  message: '',
+  style:{} //默认没有样式
+};
+
+export default RadioMultiSelect;
--- app/web/component/spa/ssr/components/vote.js
+++ app/web/component/spa/ssr/components/vote.js
@@ -11,27 +11,9 @@ import {
 	WingBlank
 } from 'antd-mobile';
 
-const CheckboxItem = Checkbox.AgreeItem
 
-const voteInfo = [ {
-	// 1单 2多
-	type: 1,
-	list: [ {
-		img: 'https://zos.alipayobjects.com/rmsportal/AiyWuByWklrrUDlFignR.png',
-		title: '1.投票题目内容投票题目内容投票题目内容投票题目内容投票题目内容'
-	}, {
-		img: 'https://zos.alipayobjects.com/rmsportal/AiyWuByWklrrUDlFignR.png',
-		title: '2.我不是内容哦你个'
-	} ]
-}, {
-	// 1单 2多
-	type: 2,
-	list: [ {
-		title: '1.投票题目内容投票题目内容投票题目内容投票题目内容投票题目内容'
-	}, {
-		title: '2.我不是内容哦你个2222222222'
-	} ]
-} ];
+import RadioMultiSelect from 'component/radio-multi-select/radio-multi-select'
+const CheckboxItem = Checkbox.AgreeItem
 
 class Vote extends Component {
 	constructor( props ) {
@@ -43,10 +25,50 @@ class Vote extends Component {
 	componentDidMount() {
 
 	}
+	componentWillMount(){
+				this.state.voteInfo  = [ {
+					// 1单 2多
+					type: 1,
+					list: [ {
+						img: 'https://zos.alipayobjects.com/rmsportal/AiyWuByWklrrUDlFignR.png',
+						title: '1.投票题目内容投票题目内容投票题目内容投票题目内容投票题目内容',
+						isActive:false
+					}, {
+						img: 'https://zos.alipayobjects.com/rmsportal/AiyWuByWklrrUDlFignR.png',
+						title: '2.我不是内容哦你个',
+						isActive:false
+					} ]
+				}, {
+					// 1单 2多
+					type: 2,
+					list: [ {
+						title: '1.投票题目内容投票题目内容投票题目内容投票题目内容投票题目内容',
+						isActive:false
+					}, {
+						title: '2.我不是内容哦你个2222222222',
+						isActive:false
+					} ]
+				} ]
+	}
+
+	checkBoxChange(index_p,index_c){
+		let {voteInfo} = this.state, parentObj = voteInfo[index_p],parentList = parentObj['list'],type = parentObj['type'];
+
+		console.log('当前type',parentObj['type'])
+		if(type== 1){
+			parentList.forEach((el)=>{
+					el.isActive = false;
+			});
+		}
+		parentList[index_c].isActive =  type == 1 ? true:!parentList[index_c].isActive;
+		this.setState({
+			voteInfo
+		  });
+		console.log('index_childdddd',index_c,'index_ppppp',index_p);
+	}
 
 	render() {
-
-
+		let {voteInfo} = this.state;
 		return (
 			<div className="main-vote-wrap">
 				<Header titleIsBlueColor={false} isBack={true} back={() => this.props.history.goBack()} title="投票"/>
@@ -54,40 +76,9 @@ class Vote extends Component {
 					<p className='title'>投票题目内容投票题目内容投票题目内容投票题目内容投票题目内容投票题目内</p>
 					<p className='desc'>补充描述补充描述补充描述补充描述补充描述补充描述补充描述补充描述补充描述补充描述补充描述补充描述补充描述补充描述补充描述补充描述</p>
 				</div>
-				{voteInfo && voteInfo.map( val => {
-					return (
-						<div className="body-content">
-							<div className="select-row-title">投票选项{val.type === 1 ? '（单选）' : '（单选）'}</div>
-							{val.list.map( (val_1,index) => {
-								let classN = 'option' + (index===val.list.length-1 ? ' last':'');
-
-								if(index===0){
-									classN += ' active';
-								}
-
-								return (
-									<div className={classN}>
-										<div className="left">
-											{val_1.img && <img className="img" src={val_1.img} alt=""/>}
-											<p className="title">{val_1.title}</p>
-											{val_1.img && (
-												<div className="note">
-													<span>摘要</span>
-													<span>介绍</span>
-													<span>更多</span>
-												</div>
-											)}
-										</div>
-										<div className="right">
-											<CheckboxItem style={{ marginTop: -20 }}/>
-										</div>
-									</div>
-								)
-							} )}
-						</div>
-					)
-				} )}
-
+				<RadioMultiSelect 
+				voteInfo = {voteInfo}
+				checkBoxChange={this.checkBoxChange.bind(this)} />
 				<WingBlank style={{padding:'30px 0'}}><Button type="primary" style={{ background: '#1AAD19' }}>投票</Button></WingBlank>
 			</div>
 
